0b22858f0aea844f47065f3c88696af8f4031f44
Ella Kummer <43374131+ellakummer@users.noreply.github.com>
2020-05-21 19:47:38 +0200

    Delete KDF_RK.h

--- implTB/KDF_RK.h
+++
@@ -1,6 +0,0 @@
-#ifndef INDCPA_H
-#define INDCPA_H
-
-int KDF_RK(unsigned char *RK[crypto_auth_hmacsha256_BYTES], unsigned char *CK[crypto_auth_hmacsha256_KEYBYTES], const unsigned char sharedkey[crypto_generichash_BYTES]);
-
-#endif
